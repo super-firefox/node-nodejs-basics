c9c39f6f370a7d9960f7cf01bc1f5a103681246c
Ihor RSS <superfirfox@gmail.com>
2022-06-04 00:45:15 +0300

    feat: done fs/create.js subtask

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,22 @@
+import path from 'path'
+import fs from 'fs/promises';
+
 export const create = async () => {
-    // Write your code here 
+    const DIR_NAME = 'files';
+    const NAME_FILE = 'fresh.txt';
+    const CONTENT = 'I am fresh and young';
+    const MSG_ERRROR = 'FS operation failed';
+
+    try {
+        const outputPath = path.resolve(DIR_NAME, NAME_FILE);
+        const dirFiles = await fs.readdir(DIR_NAME);
+
+        if(!dirFiles.includes(NAME_FILE)) {
+            fs.writeFile(outputPath, CONTENT)
+        } else {
+            throw new Error(MSG_ERRROR);
+        }
+    } catch (error) {
+        console.error(error);  
+    }
 };
