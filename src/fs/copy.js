3ee63ec1c804b98f061b7087bf07d2980cbdfa90
Ihor RSS <superfirfox@gmail.com>
2022-06-04 23:19:11 +0300

    feat: add npm script for simple run

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,14 +1,15 @@
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 async function copyFolder(from, to) {
     try {
         const dir = await fs.opendir(path.resolve(from));
         await fs.mkdir(path.resolve(to));
-        
+
         for await (const item of dir) {
             if (item.isDirectory()) {
-                copyFolder(path.resolve(from, item.name), path.resolve(to, item.name))
+                copyFolder(path.resolve(from, item.name), path.resolve(to, item.name));
             } else {
                 const f = path.resolve(from, item.name);
                 const t = path.resolve(to, item.name);
@@ -18,18 +19,18 @@ async function copyFolder(from, to) {
     } catch (error) {
         console.error(error);
     }
-
 }
 
 export const copy = async () => {
     const INPUT_DIR_NAME = 'files';
     const OUTPUT_DIR_NAME = 'files_copy';
     const MSG_ERRROR = 'FS operation failed';
+    const scriptPath = path.dirname(fileURLToPath(import.meta.url));
 
     try {
-        const dirFiles = await fs.readdir(path.resolve());
+        const dirFiles = await fs.readdir(scriptPath);
         if (!dirFiles.includes(OUTPUT_DIR_NAME) && dirFiles.includes(INPUT_DIR_NAME)) {
-            await copyFolder(path.resolve(INPUT_DIR_NAME), path.resolve(OUTPUT_DIR_NAME));
+            await copyFolder(path.resolve(scriptPath, INPUT_DIR_NAME), path.resolve(scriptPath, OUTPUT_DIR_NAME));
         } else {
             throw new Error(MSG_ERRROR);
         }
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,22 +1,26 @@
 import path from 'path'
 import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
 
 export const create = async () => {
     const DIR_NAME = 'files';
     const NAME_FILE = 'fresh.txt';
     const CONTENT = 'I am fresh and young';
     const MSG_ERRROR = 'FS operation failed';
+    const scriptPath = path.dirname(fileURLToPath(import.meta.url));
+    const outputPath = path.resolve(scriptPath, DIR_NAME, NAME_FILE);
+    const folderPath = path.resolve(scriptPath, DIR_NAME);
 
     try {
-        const outputPath = path.resolve(DIR_NAME, NAME_FILE);
-        const dirFiles = await fs.readdir(DIR_NAME);
-
+        const dirFiles = await fs.readdir(folderPath);
         if(!dirFiles.includes(NAME_FILE)) {
-            fs.writeFile(outputPath, CONTENT)
+            fs.writeFile(outputPath, CONTENT);
         } else {
             throw new Error(MSG_ERRROR);
         }
     } catch (error) {
-        console.error(error);  
+        console.error(error);
     }
 };
+
+create();
