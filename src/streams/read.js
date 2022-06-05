16742c2fbd0e675c06b4e42f7ff2167704fcf1b2
Ihor RSS <superfirfox@gmail.com>
2022-06-05 21:13:31 +0300

    feat: done stream/read.js subtask

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import process from 'process';
+
 export const read = async () => {
-    // Write your code here 
+    const INPUT_DIR = 'files';
+    const INPUT_FILE = 'fileToRead.txt';
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename);
+    const inputFilePath = path.resolve(__dirname, INPUT_DIR, INPUT_FILE);
+    const stream = fs.createReadStream(inputFilePath, 'utf-8');
+    stream.on('data', chunk => process.stdout.write(chunk));
 };
+
+read();
