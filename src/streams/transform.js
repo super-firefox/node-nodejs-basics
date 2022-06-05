b32fbb562f445fbfc87143020b25fb90badf2b18
Ihor RSS <superfirfox@gmail.com>
2022-06-05 22:58:41 +0300

    feat: done streams/transform.js subtask

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,17 @@
+import { Transform } from 'stream';
+import process from 'process';
+
+function createReversText() {
+    return new Transform({
+        transform(chunk, encode, callback) {
+            const reverse = chunk.reverse();
+            callback(null, reverse+'\n');
+        }
+    })
+}
+
 export const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(createReversText()).pipe(process.stdout);
 };
+
+transform();
