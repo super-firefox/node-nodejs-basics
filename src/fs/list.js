5300e871a2bfc5264b746b8c6dc3579eef7976f4
Ihor RSS <superfirfox@gmail.com>
2022-06-05 11:52:09 +0300

    feat: done fs/list.js subtask

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,29 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+
 export const list = async () => {
-    // Write your code here 
+    const INPUT_DIR = 'files';
+    const MSG_ERRROR = 'FS operation failed';
+    const scriptPath = path.dirname(fileURLToPath(import.meta.url));
+
+    try {
+        const rootFolder = await fs.readdir(
+            path.resolve(scriptPath)
+        );
+
+        if(rootFolder.includes(INPUT_DIR)) {
+            const dirFiles = await fs.readdir(
+                path.resolve(scriptPath, INPUT_DIR)
+            );
+            console.log(dirFiles);
+        } else {
+            throw new Error(MSG_ERRROR);
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
 };
+
+list();
