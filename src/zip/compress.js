3536baca3079cd81a09f6dcff1812a771ea7076d
Ihor RSS <superfirfox@gmail.com>
2022-06-05 23:22:30 +0300

    feat: done zlib/compress.js and zlib/decompress.js subtasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,19 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import zlib from 'zlib';
+import fs from 'fs';
+
 export const compress = async () => {
-    // Write your code here 
+    const INPUT_DIR = 'files';
+    const INPUT_FILE = 'fileToCompress.txt';
+    const OUTPUT_FILE = 'archive.gz';
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename);
+    const inputFilePath = path.resolve(__dirname, INPUT_DIR, INPUT_FILE);
+    const outputFilePath = path.resolve(__dirname, INPUT_DIR, OUTPUT_FILE);
+    const input = fs.createReadStream(inputFilePath);
+    const output = fs.createWriteStream(outputFilePath);
+    input.pipe(zlib.createGzip()).pipe(output);
 };
+
+compress();
