2fdb1bcebd2598a9d3b882aa2949d5713e90075f
Ihor RSS <superfirfox@gmail.com>
2022-06-05 22:02:52 +0300

    feat: done streams/write.js subtask

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import process from 'process';
+
 export const write = async () => {
-    // Write your code here 
+    const OUTPUT_DIR = 'files';
+    const OUTPUT_FILE = 'fileToWrite.txt';
+    const __filename = fileURLToPath(import.meta.url)
+    const __dirname = path.dirname(__filename);
+    const outputFilePath = path.resolve(__dirname, OUTPUT_DIR, OUTPUT_FILE);
+
+    const streamWrite = fs.createWriteStream(outputFilePath);
+    process.stdin.on('data',  chunk => streamWrite.write(chunk));
 };
+
+write();
