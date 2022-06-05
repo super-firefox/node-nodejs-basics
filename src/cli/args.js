bf809d3b6a92b710303882b6ca8a1efffd0b53a1
Ihor RSS <superfirfox@gmail.com>
2022-06-05 14:30:58 +0300

    feat: done cli/args.js subtask

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,21 @@
+import process from 'process';
+
+/**
+ * WARNING!!!
+ * If you use npm script then first argument must "--".
+ * Example:
+ * $ npm run cli:args -- --prop1Name value1 --prop2Name value2
+ */
 export const parseArgs = () => {
-    // Write your code here 
+    const argumentsArray = process.argv;
+    const len = argumentsArray.length;
+    for(let i = 0; i < len-1; i++) {
+        const item = argumentsArray[i];
+        if(item.startsWith('--')) {
+            const value = argumentsArray[i+1];
+            console.log(`${item.slice(2)} is ${value}${i !== len-2 ? ',': ''}`);
+        }
+    }
 };
+
+parseArgs();
